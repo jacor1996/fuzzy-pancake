2ae3478df152497edaa827fff7484fbe08f307d8
Jacek Noga <jaceknoga11@gmail.com>
2017-11-03 15:04:34 +0100

    Created DAL interface and implementation, added EF.

---
+++ fuzzy-pancake/DAL/Concrete/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete
+{
+    class DataRepository : IDataRepository
+    {
+        public IQueryable GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
