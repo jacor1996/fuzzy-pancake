546ddf30642a39b2de1e97e2c8464e579e3cd0e7
Jacek Noga <jaceknoga11@gmail.com>
2017-12-07 18:30:33 +0100

    Failed to add unit test, i will do that later

---
+++ fuzzy-pancake/WebApplication.Tests/Controllers/MealControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+using DAL;
+using DAL.Concrete;
+using DAL.DataModel;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class MealControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+
+            Assert.AreEqual(true ,true);
+        }
+    }
+}
