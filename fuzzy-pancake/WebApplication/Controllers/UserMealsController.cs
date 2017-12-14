f36cc3460e0444e0f0ac4eb15dd85e88d1292398
Jacek Noga <jaceknoga11@gmail.com>
2017-12-14 19:39:30 +0100

    Working on AddMeal Action

--- fuzzy-pancake/WebApplication/Controllers/UserMealsController.cs
+++ fuzzy-pancake/WebApplication/Controllers/UserMealsController.cs
@@ -32,7 +32,36 @@ namespace WebApplication.Controllers
             {
                 return RedirectToAction("Index");
             }
+
+            ViewBag.MealNames = CreateDropDownForMealNames(); 
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMeal(int? id, int MealNames, User_Meals userMeal)
+        {
+            userMeal.MealId = id;
+            userMeal.MealNumber = MealNames;
+            ModelState.Remove("MealId");
+            if (ModelState.IsValid)
+            {
+                
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public static List<SelectListItem> CreateDropDownForMealNames()
+        {
+            List<SelectListItem> mealNames = new List<SelectListItem>();
+            mealNames.Add(new SelectListItem { Value = "0", Text = "Breakfast"});
+            mealNames.Add(new SelectListItem { Value = "1", Text = "Lunch" });
+            mealNames.Add(new SelectListItem { Value = "2", Text = "Dinner" });
+            mealNames.Add(new SelectListItem { Value = "3", Text = "Snack" });
+            mealNames.Add(new SelectListItem { Value = "4", Text = "Supper" });
+
+            return mealNames;
         }
     }
 }
